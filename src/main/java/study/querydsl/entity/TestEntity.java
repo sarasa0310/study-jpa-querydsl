a6632ba4d6cbcfefc7bc9b09e2bb10d3a24a8ad8
jimmy <dev.sarasa.0310@gmail.com>
2023-08-31 21:37:26 +0900

    Querydsl 검증

---
+++ src/main/java/study/querydsl/entity/TestEntity.java
@@ -0,0 +1,16 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class TestEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.QTestEntity;
+import study.querydsl.entity.TestEntity;
 
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
 	void contextLoads() {
+		TestEntity testEntity = new TestEntity();
+		em.persist(testEntity);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QTestEntity qTestEntity = QTestEntity.testEntity;
+
+		TestEntity result = query
+				.selectFrom(qTestEntity)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(testEntity);
+		assertThat(result.getId()).isEqualTo(testEntity.getId());
 	}
 
 }
